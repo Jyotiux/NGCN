8662c77a8c6981db6d907e8417b9c508afa86d9b
Shashwat <shashwatp108@gmail.com>
2025-09-22 10:33:10 +0530

    Use link to for blog card

--- frontend/src/Components/HomeBlogCard/HomeBlogCard.jsx
+++ frontend/src/Components/HomeBlogCard/HomeBlogCard.jsx
@@ -1,13 +1,17 @@
 import styles from "./HomeBlogCard.module.css";
+import { Link } from "react-router-dom";
+
 
 // The component now accepts props
 function HomeBlogCard({ image, title, description, link }) {
     return (
-        <div className={styles.container} onClick={() => window.location.href = link}>
-            {/* Use the props instead of hardcoded values */}
+        // <div className={styles.container} onClick={() => window.location.href = link}>
+        <div className={styles.container}>
+                        <Link to={link}>
             <div className={styles.image}><img src={image} alt="Image" /></div>
             <div className={styles.title}><h2>{title}</h2></div>
             <div className={styles.description}><p>{description}</p></div>
+            </Link>
         </div>
     );
 }
