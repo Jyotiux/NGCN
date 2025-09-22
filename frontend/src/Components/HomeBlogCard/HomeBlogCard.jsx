5bffae458a8f98b0e2ac5625f54cc9d86d04904d
Shashwat <shashwatp108@gmail.com>
2025-09-22 10:41:59 +0530

    removed text decoration from blog cards

--- frontend/src/Components/HomeBlogCard/HomeBlogCard.jsx
+++ frontend/src/Components/HomeBlogCard/HomeBlogCard.jsx
@@ -7,7 +7,7 @@ function HomeBlogCard({ image, title, description, link }) {
     return (
         // <div className={styles.container} onClick={() => window.location.href = link}>
         <div className={styles.container}>
-                        <Link to={link}>
+                        <Link to={link} className={styles.link}>
             <div className={styles.image}><img src={image} alt="Image" /></div>
             <div className={styles.title}><h2>{title}</h2></div>
             <div className={styles.description}><p>{description}</p></div>
